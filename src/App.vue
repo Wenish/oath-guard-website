3c1ce3cd3003afcc8a87c6b27f2b135a426188da
Wenish <Wenish@users.noreply.github.com>
2025-06-30 00:08:00 +0200

    docs: add README files for Lora and Cinzel variable fonts
    Add detailed README.txt files for Lora and Cinzel fonts explaining the
    variable font axis, available static styles, and usage instructions.
    
    Include links to resources on variable fonts, installation guides for
    different platforms, and licensing information to help users
    understand and utilize the fonts effectively.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <div class="text-center">
-    <h1 class="text-3xl font-bold">Oath Guard Website</h1>
-    <p class="text-lg mt-4">This is a simple website built with Vue 3 and Vite.</p>
-    <p class="text-sm mt-2 text-gray-500">Edit <code>src/App.vue</code> to get started.</p>
-  </div>
+  <PageHome />
 </template>
+<script setup lang="ts">
+import PageHome from './pages/PageHome.vue';
+</script>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="h-[calc(60svh)] relative">
+        <img src="../assets/hero-image.webp" alt="Hero Image"
+            class="absolute w-full h-full object-cover object-top z-[-1]" />
+        <div class="absolute bottom-0 left-0 w-full flex flex-col items-center pb-24">
+            <h1
+                class="font-cizel font-bold text-9xl text-[#e46235] [text-shadow:_2px_2px_0_#000,2px_-2px_0_#000,-2px_2px_0_#000,-2px_-2px_0_#000]">
+                OATHGUARD
+            </h1>
+            <p
+                class="font-lora font-semibold uppercase [text-shadow:_1px_1px_0_#000,1px_-1px_0_#000,-1px_1px_0_#000,-1px_-1px_0_#000] text-lg pb-5">
+                Prepare to rise. Stand United. The Realm depends on your OATH.
+            </p>
+            <button class="font-cizel font-semibold uppercase bg-[#701305] text-2xl px-[2em] py-[0.5em] hover:bg-orange-800 border-[#701305] hover:border-b-rose-950 border-2 cursor-pointer text-[#e7dcbf]">join the discord</button>
+        </div>
+    </div>
+    <div class="grid">
+        <div class="bg-[#090505]">
+            <div class="max-w-2xl mx-auto text-center py-32">
+                <h2 class="font-cizel font-semibold text-6xl pb-4 text-[#e7dcbf]">What is Oathguard?</h2>
+                <p class="text-lg pb-6">Prepare to a dark-fantasy, action-rougelike game where your skill, timing and strategy are key. Survive waves of corrupted foes defend sacred relics and forage alliances in a shattered world.</p>
+                <button class="font-cizel font-semibold uppercase bg-[#701305] text-2xl px-[2em] py-[0.5em] hover:bg-orange-800 border-[#701305] hover:border-b-rose-950 border-2 cursor-pointer text-[#e7dcbf]">wishlist on steam</button>
+            </div>
+        </div>
+        <div class="max-w-2xl mx-auto text-center py-32">
+            <h2 class="font-cizel font-semibold text-6xl pb-4 text-[#e7dcbf]">Stay Updated</h2>
+            <p class="text-lg pb-6">Enter your email to get exclusive news, beta access and behind-the-scenes updates.</p>
+            <div class="flex gap-4">
+                <input type="email" name="email" autocomplete="email" placeholder="Enter your email" class="border-2 border-[#212322] p-2 px-4 w-full max-w-md text-xl" />
+                <button class="font-cizel font-semibold uppercase bg-[#701305] text-2xl px-[2em] py-[0.5em] hover:bg-orange-800 border-[#701305] hover:border-b-rose-950 border-2 cursor-pointer text-[#e7dcbf]">Subscribe</button>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="text-center py-8">
+            <img src="../assets/404ForgeLogo_spaced.webp" alt="404 Forge Logo" class="mx-auto pb-8 w-32 h-auto">
+            <p class="text-sm">Copyright © {{ new Date().getFullYear() }} - All right reserved by 404 Forge</p>
+        </div>
+    </footer>
+</template>
+<script setup lang="ts">
+</script>
